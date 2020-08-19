60994c33afbab6e3bfd80f66ab394a19194808d4
Ian Valdovinos <ian.valdovinos752001@gmail.com>
2020-08-19 10:46:07 -0400

    Make references for all the buttons in script.js

--- Frontend/Primaria_Grade1/Counting/resources/script/script.js
+++ Frontend/Primaria_Grade1/Counting/resources/script/script.js
@@ -4,6 +4,17 @@ const button_aprendiendo = document.getElementById('button-aprendiendo');
 const button_entrenando = document.getElementById('button-entrenando');
 const button_compitiendo = document.getElementById('button-compitiendo');
 
+const button_bajo = document.getElementById('button-bajo');
+const button_medio = document.getElementById('button-medio');
+const button_alto = document.getElementById('button-alto');
+
+const button_nivel= document.getElementById('button-nivel');
+const button_grado = document.getElementById('button-grado');
+const button_curso = document.getElementById('button-curso');
+const button_materia = document.getElementById('button-materia');
+const button_objetivo = document.getElementById('button-objetivo');
+
+
 const normalButtonColor = 'rgb(156, 155, 160)';
 const pressedButtonColor = 'rgb(107, 106, 108)';
 
