c906a34990c90c8694c84edf16b802539bc3a920
Ian Valdovinos <ian.valdovinos752001@gmail.com>
2020-08-18 21:34:28 -0400

    Delete anchors and create script.js file

---
+++ Frontend/Primaria_Grade1/Counting/resources/script/script.js
@@ -0,0 +1,7 @@
+// Left menu elements
+
+const button_aprendiendo = document.getElementById('button-aprendiendo');
+const button_entrenando = document.getElementById('button-entrenando');
+const button_compitiendo = document.getElementById('button-compitiendo');
+
+
